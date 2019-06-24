4e0f4602b4f2b7c0b201e75cd1081dc76b605d02
sabrinepage <50197845+sabrinepage@users.noreply.github.com>
2019-06-24 09:12:00 +0200

    sabrine
    panier presque fini

---
+++ InfoStock/src/Entity/Detail.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\DetailRepository")
+ */
+class Detail
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Commande", inversedBy="details")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $commande;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Produit", inversedBy="details")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $produit;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $qcom;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCommande(): ?Commande
+    {
+        return $this->commande;
+    }
+
+    public function setCommande(?Commande $commande): self
+    {
+        $this->commande = $commande;
+
+        return $this;
+    }
+
+    public function getProduit(): ?Produit
+    {
+        return $this->produit;
+    }
+
+    public function setProduit(?Produit $produit): self
+    {
+        $this->produit = $produit;
+
+        return $this;
+    }
+
+    public function getQcom(): ?int
+    {
+        return $this->qcom;
+    }
+
+    public function setQcom(int $qcom): self
+    {
+        $this->qcom = $qcom;
+
+        return $this;
+    }
+}
---
+++ InfoStock/src/Entity/Panier.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PanierRepository")
+ */
+class Panier
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Produit", inversedBy="paniers")
+     */
+    private $idproduit;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $session;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdproduit(): ?Produit
+    {
+        return $this->idproduit;
+    }
+
+    public function setIdproduit(?Produit $idproduit): self
+    {
+        $this->idproduit = $idproduit;
+
+        return $this;
+    }
+
+    public function getSession(): ?string
+    {
+        return $this->session;
+    }
+
+    public function setSession(string $session): self
+    {
+        $this->session = $session;
+
+        return $this;
+    }
+}
