03ad15246676104f5c142481a78011e4bdb01cac
sabrinepage <50197845+sabrinepage@users.noreply.github.com>
2019-07-05 15:34:26 +0200

    fixture admin
    ADIL : J'ai ajouté une fixture pour que l'on puisse ajouter un administrateur simplement par ligne de commande.

--- InfoStock/src/DataFixtures/AppFixtures.php
+++ InfoStock/src/DataFixtures/AppFixtures.php
@@ -3,35 +3,42 @@
 namespace App\DataFixtures;
 
 use Faker;
-use App\Entity\Produit;
+use Faker\Factory;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+public function __construct(UserPasswordEncoderInterface $encoder)
+{
+    $this->encoder = $encoder;
+}
+ 
+ 
     public function load(ObjectManager $manager)
     {
        
        
        $faker = Faker\Factory::create("fr_FR");
         
-       for($i=0; $i < 100 ; $i++){
-            $produits = new Produit();
-            $produits->setLibelle($faker->name)
-                    ->setPrix($faker->randomDigit)
-                    ->setDescription($faker->word)
-                    ->setValeursolde($faker->randomDigit)
-                    ->setImageName('yolo')
-                    
-                         ;
-
-
-
-            $manager->persist($produits);
-        }
+        $roles = array(
+            'ROLE_ADMIN'
+        );
+        $user = new User();
+        $user->setEmail('admin@admin');
+        $password = $this->encoder->encodePassword($user, 'admin');
+        $user->setPassword($password)
+            ->setRoles($roles)
+            ->setNom('Admin')
+        ;
+        $manager->persist($user);
         $manager->flush();
-       
+   
     }
 }
